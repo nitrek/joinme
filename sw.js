a32bd79482e42be1c17c407b1d5076d660eee59e
nitrek <koolestnitesh@gmail.com>
2017-05-16 05:25:50 +0530

    sw bug fix

--- sw.js
+++ sw.js
@@ -2,40 +2,98 @@
 // this polyfill adds them.
 importScripts('serviceworker-cache-polyfill.js');
 
-// Here comes the install event!
-// This only happens once, when the browser sees this
-// version of the ServiceWorker for the first time.
-self.addEventListener('install', function(event) {
-  // We pass a promise to event.waitUntil to signal how 
-  // long install takes, and if it failed
-  event.waitUntil(
-    // We open a cache…
-    caches.open('simple-sw-v1').then(function(cache) {
-      // And add resources to it
-      return cache.addAll([
-        './',
-        'style.css',
-        'logging.js'
-      ]);
-    })
-  );
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var dataCacheName = 'weatherData-v1';
+var cacheName = 'weatherPWA-step-8-1';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/home.html',
+    'hsbc.png',
+    'login.js',
+    'styles.css',
+    'style.css',
+    'images/welcome_card.jpg',
+    'images/user.jpg'
+];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
-// The fetch event happens for the page request with the
-// ServiceWorker's scope, and any request made within that
-// page
-self.addEventListener('fetch', function(event) {
-  // Calling event.respondWith means we're in charge
-  // of providing the response. We pass in a promise
-  // that resolves with a response object
-  event.respondWith(
-    // First we look for something in the caches that
-    // matches the request
-    caches.match(event.request).then(function(response) {
-      // If we get something, we return it, otherwise
-      // it's null, and we'll pass the request to
-      // fetch, which will use the network.
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    /*
+     * Fixes a corner case in which the app wasn't returning the latest data.
+     * You can reproduce the corner case by commenting out the line below and
+     * then doing the following steps: 1) load app for first time so that the
+     * initial New York City data is shown 2) press the refresh button on the
+     * app 3) go offline 4) reload the app. You expect to see the newer NYC
+     * data, but you actually see the initial data. This happens because the
+     * service worker is not yet activated. The code below essentially lets
+     * you activate the service worker faster.
+     */
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    var dataUrl = 'https://api.uber.com/v1.2/history?limit=50';
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        /*
+         * When the request URL contains dataUrl, the app is asking for fresh
+         * weather data. In this case, the service worker always goes to the
+         * network and then caches the response. This is called the "Cache then
+         * network" strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+         */
+        e.respondWith(
+            caches.open(dataCacheName).then(function(cache) {
+                return fetch(e.request).then(function(response) {
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        /*
+         * The app is asking for app shell files. In this scenario the app uses the
+         * "Cache, falling back to the network" offline strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+         */
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
 });
