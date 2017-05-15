e5ac9601136e9750b646b3b7d560b90041d4fd6f
nitrek <koolestnitesh@gmail.com>
2017-05-16 05:12:39 +0530

    interests page

--- home.js
+++ home.js
@@ -8,17 +8,3 @@ $("#myinterests").click(function() {
     console.log("ds");
     $("#mdl-grid-content").load('interests.html');
 });
-// <
-// form action = "#" >
-//
-//
-//     <
-//     label class = "mdl-textfield__label"
-// for = "sample3" > Text... < /label> <
-//     /div> <
-// //     /form>
-// html("<form action='#'><div class='mdl-textfield mdl-js-textfield mdl-textfield--floating-label'>" +
-//     "<input placeholder='enter interests'class='mdl-textfield__input' type='text' id='sample3'><button type='submit'>" +
-//     "<button type=\"button\" class=\"button buttonBlue\" id=\"submitInterests\">Login" +
-//     "<div class='ripples buttonRipples'><span class=\"ripplesCircle\"></span></div>" +
-//     "</button>)</form>  ")
