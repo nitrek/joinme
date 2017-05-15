035d978dc49f5293b7a5968f51af730d1281c9f7
nitrek <koolestnitesh@gmail.com>
2017-05-16 04:15:53 +0530

    initial commit

---
+++ login.js
@@ -0,0 +1,59 @@
+$(window, document, undefined).ready(function() {
+
+    $('input').blur(function() {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('used');
+        else
+            $this.removeClass('used');
+    });
+
+    var $ripples = $('.ripples');
+
+    $ripples.on('click.Ripples', function(e) {
+
+        var $this = $(this);
+        var $offset = $this.parent().offset();
+        var $circle = $this.find('.ripplesCircle');
+
+        var x = e.pageX - $offset.left;
+        var y = e.pageY - $offset.top;
+
+        $circle.css({
+            top: y + 'px',
+            left: x + 'px'
+        });
+
+        $this.addClass('is-active');
+
+    });
+
+    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+        $(this).removeClass('is-active');
+    });
+
+});
+$("#login").click(function() {
+    if ($("#name").val().length <= 2) {
+        $("#messages").html("Name is required");
+        $("#messages").css('color', '#df000c');
+        $("#messages").css("background", "#ffbaba");
+    } else if ($("#email").val().length <= 2 && !isValidEmailAddress($.trim($("#email").val()))) {
+        $("#messages").html("Valid Email is required");
+        $("#messages").css('color', '#df000c');
+        $("#messages").css("background", "#ffbaba");
+    } else {
+        $('#name').css('text-transform', 'lowercase');
+        $('#name').css('text-transform', 'lowercase');
+        $("#messages").html("logging In ..");
+        $("#messages").css('color', '#00e64d');
+        localStorage.setItem("currentName", $.trim($("#name").val()));
+        localStorage.setItem("currentEmail", $.trim($("#email").val()));
+        window.location.replace("home.html");
+    }
+});
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
